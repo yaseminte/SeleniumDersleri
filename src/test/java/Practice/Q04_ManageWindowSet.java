cc46ea656822a75633620661441f490dfaa48616
Yasemin Turker <yturker89@gmail.com>
2022-09-20 17:58:57 +0300

    20.09.2022

--- src/test/java/Practice/Q04_ManageWindowSet.java
+++ src/test/java/Practice/Q04_ManageWindowSet.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Q04 {
+public class Q04_ManageWindowSet {
     public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
